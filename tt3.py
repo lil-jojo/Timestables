eb098b63672ed68e8bd9fc09155328fd9f9c3c06
Roger-Heathcote <github@technicalbloke.com>
2024-01-15 20:21:49 +0000

    Fixed typo in python code

--- tt3.py
+++ tt3.py
@@ -8,8 +8,8 @@ first = random.randint(2,10)
 # think of another random number and save it in second
 second = random.randint(2,10)
 
-# do the sum and save the answer in awnser
-awnser = first * second
+# do the sum and save the answer in answer
+answer = first * second
 
 # save the current time as start
 start = time.time()
@@ -20,14 +20,14 @@ print ("")
 # ask me the question
 print (str(first) + "*" + str(second))
 
-# don't tell me the awnser until I have guessed it
-input("Enter key should be pressed when you have finished thinking of the awnser")
+# don't tell me the answer until I have guessed it
+input("Enter key should be pressed when you have finished thinking of the answer")
 
 end= time.time()
 
 # tell me the answer
-print ("The awnser is " + str(awnser))
+print ("The answer is " + str(answer))
 
 rounded_time = round(end - start, 1)
 
-print ("and it took you " + str( rounded_time ) + " seconds to awnswer")
+print ("and it took you " + str( rounded_time ) + " seconds to answer")
